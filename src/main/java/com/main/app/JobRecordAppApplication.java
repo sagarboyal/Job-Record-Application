c0e802df1b8d71bbb783fcbc222d44ca057758ad
sagarboyal <sboyal12@gmail.com>
2025-05-08 10:15:30 +0530

    initial commit!

---
+++ src/main/java/com/main/app/JobRecordAppApplication.java
@@ -0,0 +1,13 @@
+package com.main.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecordAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobRecordAppApplication.class, args);
+    }
+
+}
